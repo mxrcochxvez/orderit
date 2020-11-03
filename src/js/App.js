d461bcb3217596718bc21bae1a2c21f0d428d66f
mxrcochxvez <marcode.chavez.jr@gmail.com>
2020-11-03 00:06:50 -0800

    adding context for state and dispatch

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
+import history from './utils/history'
 import Homepage from './pages/Homepage'
 
+import reducer from './reducer'
+import StateProvider from './StateProvider'
+import DispatchProvider from './DispatchProvider'
+import Cart from './pages/Cart'
+
 function App() {
+
+  const [cart] = React.useState([])
+
+  const [state, dispatch] = React.useReducer(reducer, cart)
+
   return (
-    <Router>
-      <Route exact path="/" component={Homepage} />
+    <Router history={history}>
+      <Switch>
+        <StateProvider.Provider value={state}>
+          <DispatchProvider.Provider value={dispatch}>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/cart" component={Cart} />
+          </DispatchProvider.Provider>
+        </StateProvider.Provider>
+      </Switch>
     </Router>
   )
 }
--- src/js/pages/Homepage.js
+++ src/js/pages/Homepage.js
@@ -5,41 +5,27 @@ import ItemCard from '../components/ItemCard'
 import NavMenu from '../components/NavMenu'
 import OrderingMenu from '../components/OrderingMenu'
 
-import reducer from '../reducer'
+import items from '../../datasource/items.json'
+import StateProvider from '../StateProvider'
+import DispatchProvider from '../DispatchProvider'
 
 function Homepage() {
-
-  const [cart] = React.useState([]);
-
-  const items = [
-    {
-      name: 'One Dozen Donuts',
-      description: 'A dozen donuts guaranteed to please.',
-      imageSrc: 'https://i2.wp.com/www.eatthis.com/wp-content/uploads/2019/12/how-to-make-donuts-20.jpg?resize=640%2C360&ssl=1'
-    },
-    {
-      name: 'Donut Holes',
-      description: 'A dozen donuts guaranteed to please.',
-      imageSrc: 'https://media.istockphoto.com/photos/glazed-doughnut-holes-picture-id186882341?k=6&m=186882341&s=612x612&w=0&h=dFI-XeOsLO3kk2NBkb9c6FDjibegilHmtihrjL7JGvQ='
-    }
-  ]
-
-  const [state, dispatch] = React.useReducer(reducer, cart)
-
   return (
     <Router>
       <NavMenu />
       <Header />
       <OrderingMenu />
-      {items.map((val, i) => (
-        <ItemCard key={i} state={state} dispatch={dispatch} name={val.name} description={val.description} imageSrc={val.imageSrc} />
-      ))}
-      <p>Here's what you've got in your cart!</p>
-      <ul>
-        {state.map((val, i) => (
-          <li key={i}>{val.name}</li>
-        ))}
-      </ul>
+      <StateProvider.Consumer>
+        {(state) => (
+          <DispatchProvider.Consumer>
+            {(dispatch) => {
+              return items.map((val, i) => (
+                <ItemCard key={i} state={state} dispatch={dispatch} name={val.name} description={val.description} imageSrc={val.imageSrc} />
+              ))
+            }}
+          </DispatchProvider.Consumer>
+        )}
+      </StateProvider.Consumer>
     </Router>
   )
 }

4736776f5024e8ce7b76554a5c8a7227cf96cb01
mxrcochxvez <marcode.chavez.jr@gmail.com>
2020-11-06 12:55:33 -0800

    adding individual items and tabs now work

--- src/js/pages/Homepage.js
+++ src/js/pages/Homepage.js
@@ -1,32 +1,52 @@
 import React from 'react'
 import Header from '../components/Header'
 import ItemCard from '../components/ItemCard'
-import OrderingMenu from '../components/OrderingMenu'
 
 import items from '../../datasource/items.json'
 import StateProvider from '../StateProvider'
 import DispatchProvider from '../DispatchProvider'
 
+import { Tabs, Tab } from 'sancho'
+
 import '../../styles/Home.css'
 
 function Homepage() {
 
+  const [index, setIndex] = React.useState(0)
+  const [itemSwitch, switchItems] = React.useState(true)
+
   return (
     <div>
       <Header />
-      <OrderingMenu />
+      <Tabs value={index} onChange={i => setIndex(i)} onClick={() => switchItems(!itemSwitch)} >
+        <Tab id="you">Featured Items</Tab>
+        <Tab id="sports">Individual Items</Tab>
+      </Tabs>
       <div id="item-container">
-        <StateProvider.Consumer>
-          {(state) => (
-            <DispatchProvider.Consumer>
-              {(dispatch) => {
-                return items.map((val, i) => (
-                  <ItemCard key={i} state={state} dispatch={dispatch} name={val.name} description={val.description} imageSrc={val.imageSrc} />
-                ))
-              }}
-            </DispatchProvider.Consumer>
-          )}
-        </StateProvider.Consumer>
+        {itemSwitch ?
+          <StateProvider.Consumer>
+            {(state) => (
+              <DispatchProvider.Consumer>
+                {(dispatch) => {
+                  return items.featuredItems.map((val, i) => (
+                    <ItemCard key={i} state={state} dispatch={dispatch} name={val.name} description={val.description} imageSrc={val.imageSrc} />
+                  ))
+                }}
+              </DispatchProvider.Consumer>
+            )}
+          </StateProvider.Consumer> :
+          <StateProvider.Consumer>
+            {(state) => (
+              <DispatchProvider.Consumer>
+                {(dispatch) => {
+                  return items.individualItems.map((val, i) => (
+                    <ItemCard key={i} state={state} dispatch={dispatch} name={val.name} description={val.description} imageSrc={val.imageSrc} />
+                  ))
+                }}
+              </DispatchProvider.Consumer>
+            )}
+          </StateProvider.Consumer>
+        }
       </div>
     </div>
   )
